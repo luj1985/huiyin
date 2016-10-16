f958aa050de31d8f169a7e25408e1f19921ad0ce
Lu, Jun <luj1985@gmail.com>
2016-10-16 12:14:26 +0800

    remove fullpage.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,23 @@
 $(function() {
   const $navigator = $('header.resizable');
+  const $window = $(window);
+  const $jumbotron = $('.jumbotron');
 
-  $('#fullpage').fullpage({
-    scrollBar: true,
-    onLeave : (index, nextIndex, direction) => {
-      $navigator.toggleClass('smaller', index === 1);
-    }
+  // $('#fullpage').fullpage({
+  //   scrollBar: true,
+  //   onLeave : (index, nextIndex, direction) => {
+  //     $navigator.toggleClass('smaller', index === 1);
+  //   }
+  // });
+  $jumbotron.height($window.height());
+
+  $window.on('scroll', () => {
+    $navigator.toggleClass('smaller', $window.scrollTop() > 300);
   });
+
+  $window.on('resize', () => {
+    $jumbotron.height($(window).height());
+  })
 
   $('#home').on('click', (e) => {
     e.preventDefault();
