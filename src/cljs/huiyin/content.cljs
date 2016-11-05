03b220cea824428e94a48e08c7143e1e585a025a
Lu, Jun <luj1985@gmail.com>
2016-11-05 16:38:02 +0800

    Split data and its rendering logic from hiccup structure

--- src/cljs/huiyin/content.cljs
+++ src/cljs/huiyin/content.cljs
@@ -1,6 +1,6 @@
 (ns huiyin.content)
 
-(def members
+(def ^:const members
   [{:name "James Wo"
     :title "CEO, Huiyin Blockchain Venture"
     :avator "/images/avatar.jpg"
@@ -25,10 +25,18 @@
     :avatar "/images/avatar2.jpg"
     :attrs [{:email "andrew@purse.io"}]}])
 
-(def companies
+(def ^:const companies
   [{:name "Purse.io" :url "https://purse.io"}
    {:name "Yours network" :url "https://www.yours.network"}
    {:name "Unocoin" :url "https://www.unocoin.com"}])
+
+(def ^:const introductions
+  ["Huiyin Blockchain Venture is a premier firm that partners with innovative start-ups and entrepreneurs to help them grow and define the future of the blockchain economy.
+We provide strategic investment to bring pioneering blockchain technology ideas to life and to scale in a number of areas, including infrastructure, exchange, payment, remittance or use cases.
+We are a trusted partner with the global expertise and network to make the most of the exciting opportunities that blockchain technology presents."
+   "Huiyin Blockchain Venture builds upon the strength of its parent company, the Huiyin Group (<a href=\"http://www.huiyin.com\">www.huiyin.com</a>), which was established in 2001.
+Huiyin Group manages diversified investments in commerce, energy, medicine, agriculture, tourism, electronics, chemicals, commerce and food and beverages.
+Huiyin Group has more than 20 subsidiaries, assets of over $2 billion and more than 1,000 employees."])
 
 
 (defn jumbotron [state]
@@ -51,27 +59,23 @@
 
 (defn- display-intro []
   [:article.column
-     [:h2 "Introduction"]
-     [:p "Huiyin Blockchain Venture is a premier firm that partners with innovative start-ups and entrepreneurs to help them grow and define the future of the blockchain economy.
-We provide strategic investment to bring pioneering blockchain technology ideas to life and to scale in a number of areas, including infrastructure, exchange, payment, remittance or use cases.
-We are a trusted partner with the global expertise and network to make the most of the exciting opportunities that blockchain technology presents."]
-     [:p {:dangerouslySetInnerHTML {:__html "Huiyin Blockchain Venture builds upon the strength of its parent company, the Huiyin Group (<a href=\"http://www.huiyin.com\">www.huiyin.com</a>), which was established in 2001.
-Huiyin Group manages diversified investments in commerce, energy, medicine, agriculture, tourism, electronics, chemicals, commerce and food and beverages.
-Huiyin Group has more than 20 subsidiaries, assets of over $2 billion and more than 1,000 employees."}}]])
+   [:h2 "Introduction"]
+   (for [html introductions]
+     [:p {:dangerouslySetInnerHTML {:__html html}}])])
 
 (defn content []
   [:div.container.space-between
    [display-intro]
    [display-companies]])
 
-(defn main [state]
-  [:main
-   [jumbotron state]
-   [content state]
-   [display-members state]])
-
 (defn member [state id]
   (let [m (get members id)]
     [:main.container
      [jumbotron state]
      [:div.member "display member information here"]]))
+
+(defn main [state]
+  [:main
+   [jumbotron state]
+   [content state]
+   [display-members state]])
