5f8acb90cac4a8e96019f66eac9ef4db85ebf880
Lu, Jun <luj1985@gmail.com>
2016-11-12 22:03:49 +0800

    create package for delivery

--- package.sh
+++ package.sh
@@ -4,11 +4,9 @@ TARGET_DIR=.packages
 
 rm huiyin-*.zip
 rm -rf $TARGET_DIR
-mkdir -p $TARGET_DIR/dist
+mkdir -p $TARGET_DIR/{source-code,dist}
 
-git clone . $TARGET_DIR/source-code
-rm -rf $TARGET_DIR/source-code/.git
-
+git archive master | tar -x -C $TARGET_DIR/source-code
 boot dist -- target -d $TARGET_DIR/dist
 
 (cd $TARGET_DIR && zip -r ../huiyin-$(date +"%Y%m%d-%H%M%S") *)
