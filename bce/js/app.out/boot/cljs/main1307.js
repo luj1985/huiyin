cb3675629dfc350d710c261dcea427e70dd5f0d3
Lu, Jun <luj1985@gmail.com>
2016-11-12 20:36:28 +0800

    exclude build artifacts

--- bce/js/app.out/boot/cljs/main1307.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.8.51 {:static-fns true, :optimize-constants true}
-goog.provide('boot.cljs.main1307');
-goog.require('cljs.core');
-goog.require('huiyin.app');
-huiyin.app.init();
--- bce/js/app.out/reagent/interop.js
+++
@@ -1,3 +0,0 @@
-// Compiled by ClojureScript 1.8.51 {:static-fns true, :optimize-constants true}
-goog.provide('reagent.interop');
-goog.require('cljs.core');
